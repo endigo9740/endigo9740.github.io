40eec3213427d24cccd84e6b41bb2fd7ac689faa
endigo9740 <gundamx9740@gmail.com>
2020-01-13 16:13:06 -0600

    Drop manifest and vanta config files

--- js/vanta.config.js
+++
@@ -1,8 +0,0 @@
-var w = window.innerWidth;
-var s = w > 767 ? 17: 20;
-
-VANTA.NET({
-    el: "#vanta",
-    backgroundColor: '#fdb413', color: '#9e7417',
-    points: 10, maxDistance: 20, spacing: s, showDots: true
-});
