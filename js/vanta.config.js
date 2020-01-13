e4fb97d58133bebf6c3a3ddbb02f17aaba2dbd2c
endigo9740 <gundamx9740@gmail.com>
2020-01-13 15:39:44 -0600

    Lightning jumbotron lines

--- js/vanta.config.js
+++ js/vanta.config.js
@@ -3,6 +3,6 @@ var s = w > 767 ? 17: 20;
 
 VANTA.NET({
     el: "#vanta",
-    backgroundColor: '#fdb413', color: '#222222',
+    backgroundColor: '#fdb413', color: '#9e7417',
     points: 10, maxDistance: 20, spacing: s, showDots: true
 });
