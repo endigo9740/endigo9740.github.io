b6213e9b215e08edf23d49775cd342436ac5d0a8
endigo9740 <gundamx9740@gmail.com>
2020-01-13 12:44:43 -0600

    Mobile optomize

---
+++ js/vanta.config.js
@@ -0,0 +1,8 @@
+var w = window.innerWidth;
+var s = w > 767 ? 17: 20;
+
+VANTA.NET({
+    el: "#vanta",
+    backgroundColor: '#fdb413', color: '#222222',
+    points: 10, maxDistance: 20, spacing: s, showDots: true
+});
